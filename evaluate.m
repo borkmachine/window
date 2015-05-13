19ace9925c102430567f1686ca1d5441052e6751
Zihao Zhang <geraldzhang1994@gmail.com>
2015-05-12 18:22:49 -0700

    working progress on evaluate

--- evaluate.m
+++ evaluate.m
@@ -5,10 +5,25 @@ function evaluate(img_dir, imgname, normal_dir)
     I = im2double(imread([img_dir imgname]));
     nmap = imread([normal_dir imgname(1:end-4) '_normalmap.png']);
     nmap = process_normap(nmap);
-    imtool(nmap);
+    
     
     [candidates_mcg, ~] = im2mcg(I,'fast');
     % generate and filter bounding boxes
-    candidates_mcg
+    for id=1:length(candidates_mcg.labels)
+        mask = ismember(candidates_mcg.superpixels, candidates_mcg.labels{id});
+        mask = double(cat(3,cat(3, mask, mask), mask));        
+        box = candidates_mcg.bboxes(id,:);
+        b2 = [box(2) box(1) box(4)-box(2) box(3)-box(1)];
+        
+        cropped_normal = imcrop(nmap.*mask, b2);
+        % filter the ceiling and floor
+        if sum(sum(or(cropped_normal > 0.9 , cropped_normal < -0.9))) > 0.1*numel(cropped_normal)
+            continue
+        end
+        
+        I = insertObjectAnnotation(I, 'rectangle', b2, 'box')
+        
+        
+    end
     
 end
