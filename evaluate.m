8700f85e61789d7f5f4feef81565f65898d0ef2c
Zihao Zhang <geraldzhang1994@gmail.com>
2015-05-12 22:04:31 -0700

    end to end evaluate, not tested

--- evaluate.m
+++ evaluate.m
@@ -8,6 +8,10 @@ function evaluate(img_dir, imgname, normal_dir)
     
     
     [candidates_mcg, ~] = im2mcg(I,'fast');
+    save('temp.mat', 'candidates_mcg');
+    load('temp.mat');
+    
+    valid_ids = [];
     % generate and filter bounding boxes
     for id=1:length(candidates_mcg.labels)
         mask = ismember(candidates_mcg.superpixels, candidates_mcg.labels{id});
@@ -16,14 +20,37 @@ function evaluate(img_dir, imgname, normal_dir)
         b2 = [box(2) box(1) box(4)-box(2) box(3)-box(1)];
         
         cropped_normal = imcrop(nmap.*mask, b2);
+        cropped_normal = cropped_normal(:,:,1);
         % filter the ceiling and floor
-        if sum(sum(or(cropped_normal > 0.9 , cropped_normal < -0.9))) > 0.1*numel(cropped_normal)
+        if sum(sum(or(cropped_normal > 0.9 , cropped_normal < -0.9))) > 0.1 * numel(cropped_normal)
+            disp('filtered');
             continue
         end
         
-        I = insertObjectAnnotation(I, 'rectangle', b2, 'box')
-        
-        
+        valid_ids = [valid_ids, id];
+        I = insertObjectAnnotation(I, 'rectangle', b2, 'box');    
     end
+    
+    save('test.mat', 'valid_ids');
+    
+    commandStr = 'python /Users/myName/pathToScript/sqr.py 2';
+    
+    % run the CNN with python
+    [status, commandOut] = system(commandStr);
+    
+    load('outputVec.mat');
+    
+    
+    % generate the mask based on the result passed from classifier
+    result_mask = zeros(size(I,1), size(I,2));
+    for id = idx
+        mask = ismember(candidates_mcg.superpixels, candidates_mcg.labels{id});        
+        box = candidates_mcg.bboxes(id,:);
+        b2 = [box(2) box(1) box(4)-box(2) box(3)-box(1)];
+        result_mask = or(result_mask, mask);
+    end
+    figure(2);
+    imshow(result_mask);
+        
     
 end
