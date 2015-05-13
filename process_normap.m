72f64713604f5aef6c2c8dfeda565b7367a06504
Zihao Zhang <geraldzhang1994@gmail.com>
2015-05-12 17:29:32 -0700

    modified process normal map script

--- process_normap.m
+++ process_normap.m
@@ -1,8 +1,7 @@
 function newnorm = process_normap(Nmap)
-
-    Nmap = im2single(Nmap);
-    newnorm(:,:,1) = (2/255)* Nmap(:,:,3)-1;
-    newnorm(:,:,2) = (2/255)* Nmap(:,:,2)-1;
-    newnorm(:,:,3) = (2/255)* Nmap(:,:,1)-1;
+    Nmap = im2single(Nmap) * 255;
+    newnorm(:,:,1) = (2/255) * Nmap(:,:,3)-1;
+    newnorm(:,:,2) = (2/255) * Nmap(:,:,2)-1;
+    newnorm(:,:,3) = (2/255) * Nmap(:,:,1)-1;
 
 end
